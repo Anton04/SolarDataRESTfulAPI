5d99ad0cc8313389f275ab7f7d8ba7a56c744328
Anton04 <anton.gustafsson@tii.se>
2014-06-06 00:55:48 +0200

    Update cvs_importer.py

--- cvs_importer.py
+++ cvs_importer.py
@@ -47,7 +47,7 @@ def ParseSLBData(slb_id="h00t",start=time.time()-(24*60*60),stop=time.time()):
   #Reformat timestamps
   for i in range(0,df.shape[0]):
       if df["Time"][i].lower().find("nan") != -1:
-          del sfsdsdfs
+          df = df.drop(df.index[i])
           continue
       timestamp = time.mktime(time.strptime(df["Time"][i],"%y-%m-%d %H:%M"))
       df["Time"][i] = timestamp
