3980bd6006549b501d3a520073768696f9ec36a2
Anton04 <anton.gustafsson@tii.se>
2014-06-09 17:41:07 +0200

    Update DataFeedUniverse.py

--- DataFeedUniverse.py
+++ DataFeedUniverse.py
@@ -1,5 +1,7 @@
 #!/bin/python
-import 
+from pubsub import pub
+import json
+import mosquitto 
 
 #Abstraction layer for accessing long time storage
 class LTSLink():
@@ -25,15 +27,29 @@ class InfluxDB_LTSLink(LTSLink):
 class Mosquitto_RTSLink(RTSLink):
   def __init__(self):
     return
+  
+  
+UniverseDef = {"Name":"Soldata",
+               "ID":"12312-234234-2342",
+               "LTS TYPE":"InfluxDB"
+               "LTS DATA":{}}
 
 #Class implementing a feed universe defined by feed definitions a realtime data access and longterm strorage.
 class Universe:
-  def __init__(self,Defenition):
+  def __init__(self,Defenition="DataFeedUniverse.json"):
     if type(Defenition) == type(""):
       File = open(Defenition,"r")
-      DefDict = json.load(File)
+      self.DefDict = json.load(File)
+    elif type(Defenition) == type({}):
+      self.DefDict = Defenition
       
+    if self.DefDict["LTS TYPE"] == "InfluxDB"
+      self.LTS = 
       
+    return
+  
+  
+            
     
 #Class implementing a feed     
 class Feed:
