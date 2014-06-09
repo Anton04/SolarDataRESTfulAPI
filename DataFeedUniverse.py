ef283831caad80a4fb5946401e3d35bbd0cc15df
Anton04 <anton.gustafsson@tii.se>
2014-06-09 17:55:25 +0200

    Update DataFeedUniverse.py

--- DataFeedUniverse.py
+++ DataFeedUniverse.py
@@ -19,9 +19,63 @@ class REPLink():
     return
 
 #Class implementing access to influxDB    
-class InfluxDB_LTSLink(LTSLink):
-  def __init__(self):
-    return    
+class InfluxFeedLTSInterface(InfluxDBClient):
+  def __init__(self,config_file="influx.json"):
+
+    #Load database credentials
+    fp = open(config_file,"r")
+    self.config = json.load(fp)
+    fp.close()
+    
+    #Connect
+    InfluxDBClient.__init__(self,self.config["host"], self.config["port"], self.config["user"], self.config["password"], self.config["database"])
+
+  def GetLastTimeStamp(self,FluxId):
+
+    result = self.query('select time from \"%s\" order desc limit 1;' % FluxId, time_precision='m')
+
+    try:
+      return float(result[0]["points"][0][0])/1000.0
+    except:
+      return 0.0
+
+  def SendToInfluxDB(self,df,FeedId):
+    #Series name
+    #series = FeedId + "/raw_data" 
+    
+    rows = 0
+
+    #Save each row
+    for i in range(0,Data.shape[0]):
+      timestamp = Data.irow(i)[0]
+      column = ["time"]
+      data = [int(timestamp*1000)]
+      
+      
+      #Iterate each value and remove NANs
+      for j in range(1,Data.shape[1]):
+        if numpy.isnan(Data.iloc[i,j]):
+          continue
+          
+        #Add key
+        column.append(Data.keys()[j])
+        data.append(Data.iloc[i,j])
+
+      #If there where only nan on this row continue to next row. 
+      if len(column) == 1:
+        continue
+          
+      fdata = [{
+          "points": [data],
+          "name": FeedId,
+          "columns": column
+          }]
+
+      self.write_points_with_precision(fdata,"m")
+      
+      rows += 1
+        
+    return rows  
     
 #Class implementing access to MQTT via mosquitto    
 class Mosquitto_RTSLink(RTSLink):
@@ -32,7 +86,15 @@ class Mosquitto_RTSLink(RTSLink):
 UniverseDef = {"Name":"Soldata",
                "ID":"12312-234234-2342",
                "LTS TYPE":"InfluxDB"
-               "LTS DATA":{}}
+               "LTS DATA":{
+                  "host":"livinglab2.powerprojects.se",
+                   "port":8086,
+                   "user":"uploader",
+                   "password":"ryKkSSnveKVpUMROt8kqvZCGJXJveu8MkJO",
+                   "database":"by-id"}
+                "RTS TYPE":"MQTT"
+                "RTS DATA":{}
+              }
 
 #Class implementing a feed universe defined by feed definitions a realtime data access and longterm strorage.
 class Universe:
@@ -44,7 +106,7 @@ class Universe:
       self.DefDict = Defenition
       
     if self.DefDict["LTS TYPE"] == "InfluxDB"
-      self.LTS = 
+      self.LTS = InfluxDB_LTSLink()
       
     return
   
