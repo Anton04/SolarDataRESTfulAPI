1196b5f2a92a26f65f30b93a1946eb5d0f1c1918
Anton04 <anton.gustafsson@tii.se>
2014-06-09 16:57:28 +0200

    bugfixes

--- cvs_importer.py
+++ cvs_importer.py
@@ -36,25 +36,52 @@ def ParseSLBData(slb_id="h00t",start=time.time()-(24*60*60),stop=time.time()):
   cols = cols[2:]
   col2 = cols.insert(0,"Time")
   col2 = col2.insert(-1,"NAN")
+
+  #Remove SLB station id from key.
+  NewCols = []
+
+  for datakey in col2:
+    NewCols.append(datakey.strip(slb_id))
   
   #Set data keys as column descriptors
-  df.columns = col2
+  df.columns = NewCols
   
   #Delete trailing columns with junk. 
-  for key in df.keys()[-5:df.shape[1]-1]:
-      if key.find(slb_id) == -1:
-          del df[key]
+  #for key in df.keys()[-5:df.shape[1]-1]:
+  #    if key.find(slb_id) == -1:
+  #        del df[key]
 
   #Reformat timestamps
   for i in range(0,df.shape[0]):
-      if numpy.isnan(df["Time"][i]) or str(df["Time"][i]).lower().find("nan") != -1 :
-          df = df.drop(df.index[i])
-          continue
+    try:
       timestamp = time.mktime(time.strptime(df["Time"][i],"%y-%m-%d %H:%M"))
       df["Time"][i] = timestamp
-
+    except:
+      df = df.drop(df.index[i])
+      
   return df
   
+
+#{"TYPE":"Feed",
+# "Name":"test",
+# "ID":"23423-2423432-23432"
+# "STREAMS":[{
+#   "NAME":"MPOWc001",
+#   "RTS TYPE":"MQTT",
+#   "LTS TYPE":"INFLUXDB",
+#   "RTS ID":"/SLB/h00t/MPOWc001",
+#   "LTS ID":"23324-23432-2342/raw_data/MPOWc001",
+#   "TIMEOUT":"600000"}]
+# "METADATA":[]
+
+
+
+
+
+
+#Geterate stream defenitions. 
+def GenerateStreamDefenitions(Data,SiteIDs):
+
 
 
 
@@ -76,11 +103,11 @@ class InfluxFeedLTSInterface(InfluxDBClient):
     fp.close()
     
     #Connect
-    InfluxDBClient.__init__(self.config["host"], self.config["port"], self.config["user"], self.config["password"], self.config["database"])
+    InfluxDBClient.__init__(self,self.config["host"], self.config["port"], self.config["user"], self.config["password"], self.config["database"])
 
   def GetLastTimeStamp(self,FluxId):
 
-    result = self.query('select time,value from %s order asc limit 1;' % FluxId, time_precision='m')
+    result = self.query('select time from \"%s\" order desc limit 1;' % FluxId, time_precision='m')
 
     try:
       return float(result[0]["points"][0][0])/1000.0
@@ -89,78 +116,43 @@ class InfluxFeedLTSInterface(InfluxDBClient):
 
   def SendToInfluxDB(self,df,FeedId):
     #Series name
-    series = FeedId + "/raw_data" 
+    #series = FeedId + "/raw_data" 
+    
+    rows = 0
 
     #Save each row
     for i in range(0,Data.shape[0]):
-        timestamp = Data.irow(i)[0]
-        column = ["time"]
-        data = [int(timestamp*1000)]
-        
-        
-        #Remove NANs
-        for j in range(1,Data.shape[1]):
-          if numpy.isnan(Data.iloc[i,j]):
-            continue
-            
-          #Add key
-          column.append(Data.keys()[j])
-          data.append(Data.iloc[i,j])
-
-        #If there where only nan on this row continue to next row. 
-        if len(column) == 0:
+      timestamp = Data.irow(i)[0]
+      column = ["time"]
+      data = [int(timestamp*1000)]
+      
+      
+      #Iterate each value and remove NANs
+      for j in range(1,Data.shape[1]):
+        if numpy.isnan(Data.iloc[i,j]):
           continue
-            
-        fdata = [{
-            "points": [data],
-            "name": series,
-            "columns": column
-            }]
-    
-        self.write_points_with_precision(fdata,"m")
-        return
+          
+        #Add key
+        column.append(Data.keys()[j])
+        data.append(Data.iloc[i,j])
 
+      #If there where only nan on this row continue to next row. 
+      if len(column) == 1:
+        continue
+          
+      fdata = [{
+          "points": [data],
+          "name": FeedId,
+          "columns": column
+          }]
 
-def SendToInfluxDB(df,FeedId,config_file="influx.json"):
-    
-    #Series name
-    series = FeedId + "/raw_data" 
-    
-    #Load database credentials
-    fp = open(config_file,"r")
-    config = json.load(fp)
-    fp.close()
-    
-    #Connect
-    client = InfluxDBClient(config["host"], config["port"], config["user"], config["password"], config["database"])
-    
-    #Save each row
-    for i in range(0,Data.shape[0]):
-        timestamp = Data.irow(i)[0]
-        column = ["time"]
-        data = [int(timestamp*1000)]
+      self.write_points_with_precision(fdata,"m")
+      
+      rows += 1
         
-        
-        #Remove NANs
-        for j in range(1,Data.shape[1]):
-            if numpy.isnan(Data.iloc[i,j]):
-                continue
-            
-            #Add key
-            column.append(Data.keys()[j])
-            data.append(Data.iloc[i,j])
-            
-        fdata = [{
-            "points": [data],
-            "name": series,
-            "columns": column
-            }]
-    
-        client.write_points_with_precision(fdata,"m")
-        return
+    return rows
 
-def lastValueTime(influx_ID):
-  
+
 
 
 
@@ -174,17 +166,38 @@ if __name__ == "__main__":
   #Get all data until now + 1h
   StopTime = time.time() + 3600
 
-
-  PeriodLen = 60*60*24*7
-
   for Site in SiteIDs:
-    FeedId = "\"%s/raw_data\"" % SiteIDs[Site]
-    StartTime = Feeds.GetLastTimeStamp(FeedId)
+      FeedId = "%s/raw_data" % SiteIDs[Site]
+      StartTime = Feeds.GetLastTimeStamp(FeedId)
+      
+      if StartTime == 0:
+        print "No previous records in: " + FeedId
+        print "Starting from Okt 2013"
+        StartTime = time.mktime(time.strptime("2013-10-01","%Y-%m-%d"))
+      else:
+        print "Last record in stream: " + FeedId
+        print "at: " + time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(StartTime))
+      
+      #Start a tiny bit after the last value.
+      Current = StartTime + 0.5
+      
+      
+      PeriodLen = 60*60*24*7
+      
+      while Current < StopTime:
+          print "Reading SLB data from: " + Site 
+          print "From: " + time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(Current))
+          print "To:   " + time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(Current + PeriodLen))
+          Data = ParseSLBData(Site,Current,Current + PeriodLen)
 
-    for period in range(StartTime,StopTime,PeriodLen):
-      print "Reading SLB data from: " + Site
-      Data = ParseSLBData(Site)
-      print "Sending data to influx as: " + FeedId
-      Feeds.SendToInfluxDB(Data,FeedId)
+          #Remove duplicate
+          if Data["Time"][0] == StartTime:
+              Data = Data.drop(Data.index[0])
+          
+          Current += PeriodLen
+          print "Sending data to influx as: " + FeedId
+
+          r = Feeds.SendToInfluxDB(Data,FeedId)
+          print "%i Rows written" % r
 
 
