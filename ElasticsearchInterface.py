06dcbd59aceba0be4c632ef71886b14b885a640b
Anton04 <anton.gustafsson@tii.se>
2014-07-01 20:24:04 +0200

    Update ElasticsearchInterface.py

--- ElasticsearchInterface.py
+++ ElasticsearchInterface.py
@@ -7,6 +7,55 @@ import numpy
 from elasticsearch import Elasticsearch
 
 class ESinterface(Elasticsearch):
-  def SaveDataFrameAs(self,index,type,id_param,dataframe):
-    pass
+  def SaveDataFrameAsIndex(self,index,doc_type,df,clear = False):
+    
+    
+    #Delete all old entries?
+    if clear:
+      try:
+        self.indices.delete(index)
+      except:
+        pass
+      
+    for item in df.index:
+    
+    column = []
+    data = []
+    docs = {}
+    
+    if type(item) != type(""):
+        #print "is nan"
+        continue
+        
+    print "Processing meta for site: " + item
+    
+    for Key in df.keys():
+        
+        Value = df.loc[item][Key]
+        
+        #Skip if value is missing
+        if type(Value) != type("") and numpy.isnan(Value):
+            continue
+            
+        #Add value
+        column.append(Key)
+        data.append(Value)
+        docs[Key] = Value
+        
+        #Add time if avalible.
+        if Key == "online_since":
+            column.append("time")
+            data.append(Value)
+            
+            print "Added time key"
+    
+    #Send data to elastic search
+    res = self.index(index=index, doc_type=doc_type, id=item, body=docs)
+    if (res['created']):
+        print "Created elasticsearch entry"
+    else:
+        print "Updated elasticsearch entry"
+
+#Update indices 
+es.indices.refresh(index="solar-sites-index")
   
