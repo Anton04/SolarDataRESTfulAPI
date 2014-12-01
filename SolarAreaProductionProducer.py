9ef72f49c48347343308471102fb7d09481e5d8f
Anton Gustafsson <anton.gustafsson@tii.se>
2014-12-01 02:24:35 +0100

    t

--- SolarAreaProductionProducer.py
+++ SolarAreaProductionProducer.py
@@ -152,7 +152,7 @@ if __name__ == "__main__":
     now = time.time()
     
     for id in areas.columns: 
-        print u"Area: %s" % areas[id]["Name"]
+        print u"Area: %s" % (areas[id]["Name"]).encode('utf-8')
         #print "processing             \r",
         #time.sleep(0.5)
         CalculateAreaProduction(areas[id])
