101e7d893eee46112c4fa93fbd06f244831d8350
Anton04 <anton.gustafsson@tii.se>
2015-02-28 17:07:23 +0100

    Update PostInfluxMeterEvent.py

--- PostInfluxMeterEvent.py
+++ PostInfluxMeterEvent.py
@@ -30,86 +30,8 @@ def SecToHMS(sec):
 
 # <codecell>
 
-def GetRawStatusFrame():
 
-    #Sites = LogDB.ListSeries()
-    res = es.GetHitsAsDataFrame(index="solar-sites-index", doc_type='meta-data')
-    
-    Sites = res.columns
-    
-    print "Procssing"
-    
-    
-    
-    df = pd.DataFrame()
-    df.index.name = "Sites"
-    
-    now = time.time() *1000
-    
-    for site in Sites:
-        print "*",
-        sys.stdout.flush()
-        
-        df.loc[site,"OpID"] = res[site]["Operator_ID"]
-        
-        df.loc[site,"LogStart"] = LogDB.GetFirstTimestamp(site)
-        df.loc[site,"LogStop"] = LogDB.GetLastTimestamp(site)
-        
-        df.loc[site,"ProdStart"]  = ProductionDB.GetFirstTimestamp(site) 
-        df.loc[site,"ProdStop"] = ProductionDB.GetLastTimestamp(site)
-    
-    
-    df["LogLag"] = now - df["LogStop"]
-    df["ProdLag"] = now - df["ProdStop"]
-    
-    print "\rDone!                "
-    sys.stdout.flush()
-    
-    
-    return df
 
-# <codecell>
-
-def FormatStatusFrame(df):
-    df2 = pd.DataFrame()
-    
-    df2["OpID"] = df["OpID"]
-    
-    df2["LogLag"] = (df.LogLag/1000).apply(SecToHMS)
-    df2["LogStop"] = (df.LogStop/1000).apply(EpocToDate)
-    df2["LogStart"] = (df.LogStart/1000).apply(EpocToDate)
-    
-    df2["ProdLag"] = (df.ProdLag/1000).apply(SecToHMS)
-    df2["ProdStop"] = (df.ProdStop/1000).apply(EpocToDate)
-    df2["ProdStart"] = (df.ProdStart/1000).apply(EpocToDate)
-    
-    df2 = df2.reindex_axis(df.columns, axis=1)
-    
-    return df2
-
-# <codecell>
-
-def GetStatusFrame():
-    return FormatStatusFrame(GetRawStatusFrame())
-
-# <codecell>
-
-def UpdateStatusWebpage(filename = "/var/www/html/status_slb.html"):
-    
-    status_raw = GetRawStatusFrame()
-    status = FormatStatusFrame(status_raw.sort("LogLag"))
-    
-    html = status.to_html(max_rows=40)
-    
-    file = open(filename,"w")
-    
-    file.write(html)
-    
-    file.close()
-    
-    #print status
-    
-    print "Status Written to %s" % filename
     
 
 # <codecell>
@@ -123,8 +45,8 @@ if __name__ == '__main__':
     #parser.add_argument('-m', dest='message', default="", help='MQTT message to process')
     
     parser.add_argument('-c', dest='credentials_file', default="/home/iot/repos/SolarDataRESTfulAPI/influxInterfaceCredentials2.json", help='Credential file')    
-    parser.add_argument('-d', dest='database', default="mqtt", help='database')
-    parser.add_argument('-s', dest='series', default="", help='series')
+    parser.add_argument('-d', dest='database', help='database')
+    parser.add_argument('-s', dest='series', help='series')
     parser.add_argument('-p', dest='power_key', default="power", help='name of power key')
     parser.add_argument('-e', dest='energy_key', default="energy", help='name of energy key')
 
